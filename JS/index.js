056b4f209dbb113de75d47d0e1b01262742903f5
Yuni <yshuh0126@gmail.com>
2019-05-06 13:06:24 -0700

    Update untracked files

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBxb3hn6W44rYdwKBrIuDYrwRRAzWz_vmc",
+    authDomain: "test-b1607.firebaseapp.com",
+    databaseURL: "https://test-b1607.firebaseio.com",
+    projectId: "test-b1607",
+    storageBucket: "test-b1607.appspot.com",
+    messagingSenderId: "1086932596636",
+    appId: "1:1086932596636:web:ebb10fad65864e36"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
